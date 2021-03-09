49bae1033fce989b66d559ff9d7274ba0c43b1fe
sanoopraj.ps@in.verizon.com <sanoopraj.ps@in.verizon.com>
2021-03-10 00:51:56 +0530

    tests

--- WaterTankMonitor-Project/Arduino/rfReciever.c
+++ WaterTankMonitor-Project/Arduino/rfReciever.c
@@ -9,20 +9,24 @@ RH_ASK rf_driver;
 // with the arduino pin number it is connected to
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-int i;    //and integer used to count
-int relayPin = 8; //Relay to control the motor
-int motorFlag = 0; //0: Not running, 1: Running
+uint8_t i;    //and integer used to count
+uint8_t relayPin = 8; //Relay to control the motor
+uint8_t motorFlag = 0; //0: Not running, 1: Running
 //Water Tank variables
 const float tankCapacity = 750.00; //Value in liter
 const float tankHeight = 93.00; //Value in Centimeter
-float waterLevel=0;
+
+float waterLevel = 0;
 //RF Receiver specific config
 // Set buffer to size of expected message
 uint8_t buf[6];
 uint8_t buflen = sizeof(buf);
 float f_recvalue, s_recvalue, l_recvalue;
 
-// Function to capture RF data from the Tx. This will be called by both setup and loop.
+/*
+* Function to capture RF data from the Tx. 
+* This will be called by both setup and loop.
+*/
 float captureRfData() {
   delay(2000);
   if (rf_driver.recv(buf, &buflen))
@@ -30,17 +34,66 @@ float captureRfData() {
     // Message received with valid che-cksum
     // Convert string to float
     float f_recvalue = atof((char*)buf);
-    Serial.print("Function: Tx Message Received: ");
+    Serial.print("captureRfData: Tx Message Received: ");
     Serial.println(f_recvalue);
     return f_recvalue;
   }
   else
     {
-    Serial.print("Function: Invalid message received from Tx\n");
+    Serial.println("captureRfData: No response from Tx");
     //lcd.print ("Function: Tx ERROR!");
     delay (1000);
   }
 }
+
+/*
+* Fuction checks in a loop to see if sensor value is correct.
+* It returns 0 for failure and 1 for success
+*/
+
+int confirmSensorLoop(int thresholdVal, char OperatorL[]){
+  float conf_recvalue;
+  int conf_count = 0;
+  int conf_error=0;
+
+   while (conf_count < 5) {
+      conf_recvalue = captureRfData();
+      if (conf_recvalue == NULL){
+        conf_error++;
+        Serial.println("confirmSensorLoop: NULL Value received from Tx");
+      }
+      if (strcmp(OperatorL, "gt") == 0) {
+        if (conf_recvalue > thresholdVal && conf_recvalue != NULL) {
+          Serial.print("confirmSensorLoop: Sensor value satisfied for threshold:- ");
+        }
+        else {
+          Serial.print("confirmSensorLoop: invalid sensor value for threshold:- ");
+          conf_error++;
+        }
+      }
+
+      if (strcmp(OperatorL, "lt") == 0) {
+        if (conf_recvalue < thresholdVal && conf_recvalue != NULL) {
+          Serial.print("confirmSensorLoop: Sensor value satisfied for threshold:- ");
+        }
+        else {
+          Serial.print("confirmSensorLoop: invalid sensor value for threshold:- ");
+          conf_error++;
+        }
+      }
+      Serial.println(conf_recvalue);
+      conf_count++;
+    }
+
+    if (conf_error > 2) {
+      Serial.println("confirmSensorLoop: Confirmation condition not satified. Failing Operation!");
+      return 0;
+    }
+    else {
+      return 1;
+    }
+}
+
 
 // Setup function
 void setup()
@@ -59,37 +112,18 @@ Serial.begin(9600);
 // Loop to check whether Trasmitter is sending data continuously
 int wcount = 0;
 int errcount = 0;
+int result_setup = 2;
+const int totalThreshold = 100; //Total capacity of the tank
 lcd.clear();
-Serial.print("Setup: Initializing!!\n");
+Serial.println("Setup: Initializing!!");
 lcd.print("Initializing!");
-while (wcount < 5){
-  Serial.print("\n");
-  Serial.print("Setup: Inside While:");
-  Serial.print(wcount);
-  Serial.print("\n");
-  //delay (2000);
-  s_recvalue = captureRfData();
-  if (s_recvalue == NULL){
-    errcount++;
-    Serial.print ("Setup: NULL Value received from Tx\n");
-  }
-  if (s_recvalue < 100.00 ){
-    Serial.print("Setup: Tx Message Received: :-");
-    Serial.print(s_recvalue);
-    Serial.print("\n");
-  }
-  else {
-    Serial.print("Setup: ERROR!. Received invalid data from the Transmitter\n");
-    Serial.print (s_recvalue);
-    Serial.print ("Stopping Arduino at Setup stage. Fix the transmitter issue and reset Arduino.\n");
-    errcount++;
-    if (errcount > 2) {
-      lcd.clear();
-      lcd.print ("Setup: ERROR!");
-      for (;;);
-    }
-  }
-  wcount++;
+static const char Operation_setup[] = "lt";
+result_setup = confirmSensorLoop(totalThreshold, Operation_setup);
+if (result_setup == 0){
+  Serial.println("Setup: Stopping Arduino. Fix the Tx issue and reset Arduino.");
+  lcd.clear();
+  lcd.print ("Setup: Tx ERROR!");
+  for (;;);
 }
 Serial.print("Setup: Successfully initialized!\n\n");
 lcd.clear();
@@ -99,43 +133,67 @@ delay(1000);
  
 void loop()
 {
-// set the cursor to column 0, line 1
-// (note: line 1 is the second row, since counting begins with 0):
-//lcd.setCursor(0, 0);
-// print the number of seconds since reset:
-//lcd.print(millis() / 1000);
 l_recvalue = captureRfData();
 // Calculate Water tank Capacity
-waterLevel = (tankCapacity/tankHeight) * l_recvalue;
-Serial.print ("loop: Water Level Calculated:- ");
+waterLevel = tankCapacity - ((tankCapacity/tankHeight) * l_recvalue);
+Serial.print ("loop: Water Level:- ");
 Serial.print (waterLevel);
-Serial.print ("Liter \n");
+Serial.print ("L \n");
 lcd.clear();
 lcd.setCursor(0,0);
-lcd.print("Water Level:- ");
+lcd.print("Water :- ");
 static char s_waterLevel[5];
-lcd.setCursor(13,0);
+lcd.setCursor(9,0);
 dtostrf(waterLevel, 5,2,s_waterLevel);
 lcd.write(s_waterLevel);
+lcd.setCursor(15,0);
+lcd.print("L");
+int result_loop = 2;
+const int lowThreshold = 70; //When the tank is empty, more distance to tank
+const int highThreshold = 20; //When the tank is full, less distance to reach water
 
-if (l_recvalue < 20) {
-  lcd.setCursor (0,1);
-  lcd.print ("Stopping!");
-  Serial.print("loop: Stopping the motor as it reached 10cm close.\n");
-  digitalWrite (relayPin, LOW);
-  motorFlag = 0;
+if (l_recvalue < highThreshold) {
+  if (motorFlag == 1) {
+    static const char Operation_loop_ht[] = "lt";
+    result_loop = confirmSensorLoop(lowThreshold, Operation_loop_ht); 
+  }
+  if (result_loop == 1 && motorFlag == 1) {
+    lcd.setCursor (0,1);
+    lcd.print ("Stopping!");
+    Serial.print("loop: Stopping the motor as it reached 10cm close.\n");
+    digitalWrite (relayPin, LOW);
+    motorFlag = 0;
+  }
+  else if (motorFlag == 0) {
+    Serial.println("loop: Continue keeping the motor idle as the distance still <20cm");
+  }
+  else {
+    Serial.println("loop: False sensor value. Continue in the same state.");
+  }
 }
-else if (l_recvalue > 70){
-  lcd.setCursor (0,1);
-  lcd.print ("Running!");
-  Serial.print("loop: Starting the motor as it reached 70cm long\n");
-  digitalWrite (relayPin, HIGH);
-  motorFlag = 1;
+else if (l_recvalue > lowThreshold){
+  if (motorFlag == 0) {
+    static const char Operation_loop_lt[] = "gt";
+    result_loop = confirmSensorLoop(lowThreshold, Operation_loop_lt);
+  }
+  if (result_loop == 1 && motorFlag == 0) {
+    lcd.setCursor (0,1);
+    lcd.print ("Running!");
+    Serial.print("loop: Starting the motor as it reached 70cm long\n");
+    digitalWrite (relayPin, HIGH);
+    motorFlag = 1;
+  }
+  else if (motorFlag == 1) {
+    Serial.println("loop: Continue running the motor as the distance still >70cm");
+  }
+  else {
+    Serial.println("loop: False sensor value. Continue in the same state.");
+  }
 }
 else {
   lcd.setCursor (0,1);
-  lcd.print ("Normal: Running!");
-  Serial.print("loop: Running the motor, and water level is normal\n");
+  lcd.print ("Normal!");
+  Serial.print("loop: Water level is normal. Persisting the previous state!\n");
 }
 
 if (motorFlag == 1) {
@@ -148,48 +206,4 @@ else {
   Serial.print("loop: ERROR:: Something wrong in getting motorStatus.");
   Serial.print(motorFlag);
 }
-
-// Check if received packet is correct size
-/*
-if (rf_driver.recv(buf, &buflen))
-{
-    // Message received with valid che-cksum
-    // Convert string to float
-    float recvalue = atof((char*)buf);
-    Serial.print("Message Received: ");
-    Serial.println(recvalue);
-   // lcd.setCursor(0, 1);
-    //lcd.print("Normal   :");    
-    //lcd.setCursor(9,1);  
-    
-    for (i = 0; i < buflen; i++)
-      { 
-        lcd.write(buf[i]); 
-                    
-      }
-        
-      // lcd.print((char)223);
-      if (recvalue < 10.00)
-    {
-        lcd.setCursor (0,1);
-        lcd.print ("Stopping!");
-        Serial.print("Making relay pin HIGH: ");
-        digitalWrite (relayPin, HIGH);
-        delay (2000);
-        digitalWrite (relayPin, LOW);
-    }
-    else if (recvalue > 50.00)
-    {
-        lcd.setCursor (0,1);
-        lcd.print ("Running!");
-        
-      }
-  }
-  else
-  {
-    Serial.print("Transmitter ERROR!");
-    lcd.print ("Transmitter ERROR!");
-    delay (1000);
-  }
-  */
 }
